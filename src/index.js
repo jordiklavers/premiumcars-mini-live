3416c1ce37934c8fac33e7b7d3c33610531d1519
Jordi Klavers <jordiklavers@gmail.com>
2025-02-28 14:10:12 +0100

    swiper

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import gsap from 'https://cdn.skypack.dev/gsap';
 import swiper from 'https://cdn.skypack.dev/swiper';
 
-
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(Draggable, InertiaPlugin);
   
